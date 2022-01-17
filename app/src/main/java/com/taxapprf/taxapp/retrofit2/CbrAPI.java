1e3bde8eb470013d58e2c24bb6d89220ebe9f97b
Helbul <bulina.nn@gmail.com>
2022-01-17 14:58:40 +0300

    + retrofit2

---
+++ app/src/main/java/com/taxapprf/taxapp/retrofit2/CbrAPI.java
@@ -0,0 +1,11 @@
+package com.taxapprf.taxapp.retrofit2;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface CbrAPI {
+
+    @GET("XML_daily.asp")
+    Call<Currencies> loadCurrencies(@Query("date_req") String date);
+}
