34eeebcbb3e428d79ced6c07f3e71f410b2e69a0
Fix <zfix.amursk@ya.ru>
2023-07-27 20:35:15 +1000

    Индикация загрузки в LoginActivity.kt

--- app/src/main/java/com/taxapprf/taxapp/ui/LoginActivity.kt
+++ app/src/main/java/com/taxapprf/taxapp/ui/LoginActivity.kt
@@ -2,17 +2,23 @@ package com.taxapprf.taxapp.ui
 
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.taxapprf.taxapp.R
+import com.taxapprf.taxapp.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity(R.layout.activity_login) {
+    val binding by viewBinding(ActivityLoginBinding::bind)
     val viewModel by viewModels<MainViewModel>()
 
-    fun onLoadingStart() {}
+    fun onLoadingStart() {
+        binding.loading.isVisible = true
+    }
 
     fun onLoadingStop() {
-
+        binding.loading.isVisible = false
     }
 
     fun onLoadingError(stringResId: Int) {
