ff2c78332297b39fa568bbcf5a0e4314633e43a0
Helbul <bulina.nn@gmail.com>
2023-09-11 14:20:02 +0300

    UI - converter

--- app/src/main/java/com/taxapprf/taxapp/ui/currency/converter/CurrencyConverterFragment.kt
+++ app/src/main/java/com/taxapprf/taxapp/ui/currency/converter/CurrencyConverterFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.taxapprf.taxapp.R
@@ -61,34 +62,25 @@ class CurrencyConverterFragment : BaseFragment(R.layout.fragment_currency_conver
                 }
             }
 
-        binding.spinnerCurrencyConverterSum.onItemSelectedListener = object :
-            AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                currenciesAdapter.getItem(position)?.let {
-                    viewModel.currency = it
+
+        binding.spinnerCurrencyConverterSum.onItemClickListener =
+            AdapterView.OnItemClickListener {
+                    adapterView, view, position, id ->
+
+                adapterView.getItemAtPosition(position)?.let {
+                    viewModel.currency = adapterView.getItemAtPosition(position).toString()
                     val newSum = binding.editCurrencyConverterSum.text.toString()
                     if (newSum != "") viewModel.setSum(newSum.toDouble())
                 }
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
-    }
+}
 
     private fun prepCurrencies() {
         val currencies = resources.getStringArray(R.array.transaction_currencies)
-        currenciesAdapter =
-            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, currencies)
-        currenciesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerCurrencyConverterSum.adapter = currenciesAdapter
-        binding.spinnerCurrencyConverterSum.setSelection(
-            currencies.indexOf(resources.getString(R.string.transaction_currency_usd))
-        )
+
+        currenciesAdapter = ArrayAdapter(requireContext(), R.layout.list_item, currencies)
+        (binding.spinnerCurrencyConverterSum as? AutoCompleteTextView)?.setAdapter(currenciesAdapter)
     }
 
     override fun onLoadingRetry() {
