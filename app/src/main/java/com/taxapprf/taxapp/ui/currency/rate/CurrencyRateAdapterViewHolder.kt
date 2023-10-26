907da1e8da8a3c8bac095bed61e35cbb3d8a1210
Fix <zfix.amursk@ya.ru>
2023-10-26 10:05:53 +0300

    UPDATE обновление списка валют под общий дизайн

--- app/src/main/java/com/taxapprf/taxapp/ui/currency/rate/CurrencyRateAdapterViewHolder.kt
+++ app/src/main/java/com/taxapprf/taxapp/ui/currency/rate/CurrencyRateAdapterViewHolder.kt
@@ -2,20 +2,24 @@ package com.taxapprf.taxapp.ui.currency.rate
 
 import androidx.recyclerview.widget.RecyclerView
 import com.taxapprf.domain.currency.CurrencyRateModel
+import com.taxapprf.taxapp.R
 import com.taxapprf.taxapp.databinding.FragmentCurrencyRateAdapterItemBinding
+import com.taxapprf.taxapp.ui.toAppDouble
 
 class CurrencyRateAdapterViewHolder(
     private val binding: FragmentCurrencyRateAdapterItemBinding,
     private val currencyNames: List<String>,
 ) : RecyclerView.ViewHolder(binding.root) {
     private lateinit var currencyRate: CurrencyRateModel
-
     fun bind(currencyRateModel: CurrencyRateModel) {
         currencyRate = currencyRateModel
 
-        binding.textCurrencyRateName.text = currencyNames[currencyRate.currency.ordinal]
-        binding.textCurrencyRateNumCode.text = currencyRate.currency.numCode
-        binding.textCurrencyRateCharCode.text = currencyRate.currency.name
-        binding.textCurrencyRateRate.text = currencyRate.rate.toString()
+        binding.textViewCurrencyRateName.text = binding.root.context.getString(
+            R.string.currency_rate_title_with_num_code,
+            currencyNames[currencyRate.currency.ordinal],
+            currencyRate.currency.numCode
+        )
+        binding.textViewCurrencyRateCharCode.text = currencyRate.currency.name
+        binding.textViewCurrencyRateRate.text = currencyRate.rate?.toAppDouble() ?: "0.00"
     }
 }
